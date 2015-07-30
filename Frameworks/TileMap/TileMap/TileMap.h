1c5774bbe0ebbb1684bac3d783a5effbbacb0b17
Andrew Molloy <amolloy@gmail.com>
2015-07-30 07:21:49 -0400

    Initial import of the TileMap framework project

---
+++ Frameworks/TileMap/TileMap/TileMap.h
@@ -0,0 +1,19 @@
+//
+//  TileMap.h
+//  TileMap
+//
+//  Created by Andrew Molloy on 7/30/15.
+//  Copyright © 2015 Andrew Molloy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TileMap.
+FOUNDATION_EXPORT double TileMapVersionNumber;
+
+//! Project version string for TileMap.
+FOUNDATION_EXPORT const unsigned char TileMapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TileMap/PublicHeader.h>
+
+
---
+++ Frameworks/TileMap/TileMapTests/TileMapTests.swift
@@ -0,0 +1,36 @@
+//
+//  TileMapTests.swift
+//  TileMapTests
+//
+//  Created by Andrew Molloy on 7/30/15.
+//  Copyright © 2015 Andrew Molloy. All rights reserved.
+//
+
+import XCTest
+@testable import TileMap
+
+class TileMapTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
