98cd58310f9865b89394066f4e8068b9b9b46f52
Andrew Molloy <amolloy@gmail.com>
2015-08-14 12:18:57 -0400

    Add TileMap to LATW and make a simple test which renders a map and lets you drag the mouse around it

--- LATW/LATW/GameScene.swift
+++ LATW/LATW/GameScene.swift
@@ -7,32 +7,73 @@
 //
 
 import SpriteKit
+import TileMap
 
-class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
-    }
-    
-    override func mouseDown(theEvent: NSEvent) {
-        /* Called when a mouse click occurs */
-        
-        let location = theEvent.locationInNode(self)
-        
-        let sprite = SKSpriteNode(imageNamed:"Spaceship")
-        sprite.position = location;
-        sprite.setScale(0.5)
-        
-        let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-        sprite.runAction(SKAction.repeatActionForever(action))
-        
-        self.addChild(sprite)
-    }
+class GameScene: SKScene
+{
+	lazy var tileMap : TileMap? =
+	{
+		let bundle = NSBundle(forClass: GameScene.self)
+		guard let testMapPath = bundle.pathForResource("Test", ofType: "fmp") else
+		{
+			return nil
+		}
+
+		guard let tileMapFile = TileMap(path: testMapPath) else
+		{
+			return nil
+		}
+
+		do
+		{
+			try tileMapFile.open()
+		}
+		catch let e
+		{
+	//		XCTFail("tileMapFile.open() threw \(e)")
+		}
+		do
+		{
+			try tileMapFile.loadChunks()
+		}
+		catch let e
+		{
+		//	XCTFail("tileMapFile.loadChunks() threw \(e)")
+		}
+		return tileMapFile
+	}()
+
+    override func didMoveToView(view: SKView)
+	{
+		guard let tileMap = self.tileMap else
+		{
+			assertionFailure("Couldn't load tile map.")
+			return
+		}
+		let renderer = TileMapSKRenderer(tileMap: tileMap)
+		let node : TileMapSKNode?
+		do
+		{
+			node = try renderer.node()
+		}
+		catch
+		{
+			assertionFailure("Couldn't create SKNode from TileMap")
+			return
+		}
+		if let node = node
+		{
+			addChild(node)
+		}
+	}
+
+	override func mouseDragged(theEvent: NSEvent)
+	{
+		if let node = children.first
+		{
+			node.position = CGPoint(x: node.position.x + theEvent.deltaX, y: node.position.y - theEvent.deltaY)
+		}
+	}
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
