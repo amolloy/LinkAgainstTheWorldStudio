5b2d3c6010dc8071241e6ef53a4fed6bd49534d9
Andrew Molloy <amolloy@gmail.com>
2016-01-02 09:58:49 -0500

    Initial import of the LATWMap framework (just xcode's template at this point).

---
+++ Frameworks/LATWMap/LATWMap/LATWMap.h
@@ -0,0 +1,19 @@
+//
+//  LATWMap.h
+//  LATWMap
+//
+//  Created by Andrew Molloy on 1/2/16.
+//  Copyright © 2016 Andy Molloy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LATWMap.
+FOUNDATION_EXPORT double LATWMapVersionNumber;
+
+//! Project version string for LATWMap.
+FOUNDATION_EXPORT const unsigned char LATWMapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LATWMap/PublicHeader.h>
+
+
