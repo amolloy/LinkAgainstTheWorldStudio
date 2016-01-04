989c937fc2c1b8b34118695f68c4d66c9e61874d
Andrew Molloy <amolloy@gmail.com>
2016-01-04 09:36:45 -0500

    Add a framework to hold some image-related functions / extensions. Currently contains a function to create a CGImage from the contents of a CGBitmapContext.

---
+++ Link Against the World Studio/ImageUtilities/ImageUtilities.h
@@ -0,0 +1,19 @@
+//
+//  ImageUtilities.h
+//  ImageUtilities
+//
+//  Created by Andrew Molloy on 1/3/16.
+//  Copyright © 2016 Andy Molloy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ImageUtilities.
+FOUNDATION_EXPORT double ImageUtilitiesVersionNumber;
+
+//! Project version string for ImageUtilities.
+FOUNDATION_EXPORT const unsigned char ImageUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageUtilities/PublicHeader.h>
+
+
