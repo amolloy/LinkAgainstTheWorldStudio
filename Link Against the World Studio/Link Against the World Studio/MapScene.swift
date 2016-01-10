100b845e0f7f30547db3d68039493bf07a6acf67
Andrew Molloy <amolloy@gmail.com>
2016-01-10 10:01:51 -0500

    Add a SpriteKit-based renderer for LATW maps.

---
+++ Link Against the World Studio/Link Against the World Studio/MapScene.swift
@@ -0,0 +1,9 @@
+//
+//  MapScene.swift
+//  Link Against the World Studio
+//
+//  Created by Andrew Molloy on 1/10/16.
+//  Copyright © 2016 Andy Molloy. All rights reserved.
+//
+
+import Foundation
